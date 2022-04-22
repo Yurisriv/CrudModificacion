778a2da2d151accdeef73b193b0a5e9a99b7d53d
Yuris <Yuririvero96@gmail.com>
2022-04-21 23:20:18 -0500

    Se agrego el campo ciudad

---
+++ src/sistema/Sistema.java
@@ -0,0 +1,25 @@
+/*
+ Creación del paquete sistema
+ */
+package sistema;
+
+import sistema.empleadosBL.empleadosBL;
+import sistema.empleadosGUI.frmEmpleados;
+
+/**
+ * Se implementa la clase sistema
+ * @author Daniel
+ */
+public class Sistema {
+
+    /**
+     * Argumentos generados a la clase principal por parte de netbeans
+     * @param args the command 
+     */
+    public static void main(String[] args) {
+         empleadosBL EM=new empleadosBL();
+        frmEmpleados E=new frmEmpleados();
+        E.setVisible(true);
+    }
+    
+}
---
+++ src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.empleadosBL;
+
+/**
+ *Creamos los métodos setters y getters de cada elemento
+ * @author Daniel
+ */
+public class empleadosBL {
+    
+    int ID;
+    String nombre, correo, Ciudad;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(String Ciudad) {
+        this.Ciudad = Ciudad;
+    }
+    
+    
+    
+}
